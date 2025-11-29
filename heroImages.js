2ed3a6159b7db69130e32ffd0201dacd5ed19399
getheardio <gkitchen@getheard.co.uk>
2025-11-29 16:04:54 +0000

    Update heroImages.js with new image URL

--- heroImages.js
+++ heroImages.js
@@ -2,7 +2,8 @@
 
 // Array of image URLs
 const images = [
-    'carlos-muza-unsplash.jpg',
+//   'carlos-muza-unsplash.jpg',
+    'dillon-shook-unsplash.jpg'
 //    'nasa-unsplash.jpg',
 //   'javier-miranda-unsplash.jpg',
 //    'markus-spiskeI-unsplash.jpg'
@@ -17,6 +18,7 @@ function changeHeroImage() {
 
 // Initial image load
 changeHeroImage();
+
 
 
 
